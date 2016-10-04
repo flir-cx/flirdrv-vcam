1754ce9869b3657b5acca2ea6598a5e999c01071
Felix Hammarstrand <felix.hammarstrand@flir.se>
2016-10-04 11:07:14 +0200

    New fpga have a new mipi clock recovery mechanism When we exits standby, we want to force a fpga mipi clock recovery We do this by disable and enable vcam mipi transfers This is done when we change/reapply fov settings
    CDP-777 / EV-2766
    
    Revert "OV5640: do not set same fov twice"
    
    This reverts commit 951ef1ad23b79da9309a77b238f1662c154c4262.

--- OV5640.c
+++ OV5640.c
@@ -33,7 +33,7 @@ typedef enum {CAM_1, CAM_2, CAM_ALL} CAM_NO;
 // Local variables
 static BOOL bCamActive[CAM_ALL] = { TRUE, FALSE };
 
-static int vcamFOV = -1;
+static int vcamFOV;
 static CAM_NO g_camera = CAM_ALL;
 struct reg_value {
     u16 u16RegAddr;
@@ -1573,10 +1573,7 @@ static void nightmode_on_off_work(struct work_struct *work)
 
 static BOOL OV5640_set_fov(PCAM_HW_INDEP_INFO pInfo,CAM_NO cam,int fov)
 {
-    int ret = 0;
-    if(vcamFOV == fov)
-        return ret;
-
+    int ret;
     OV5640_stream_off(pInfo,cam);
 
     switch(fov)
