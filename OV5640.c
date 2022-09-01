flir-cx/flirdrv-vcam

commit 584c5adc516d053064cc012a6c2dc3289b7280cf

EOWYN-2407 VCAM Configuration
Added return codes to (some..) functions, to allow reporting if the
vcam exists or not, instead of trying to load the hardware when it
does not exists, to avoid the module from trying to initialize the
vcam if it does not exist

Changed prototype for initCamera and OV5640_Init, move away from
PCAM_HW_INDEP_INFO in protype, instead opting to use struct
device *dev and extracting the PCAM_HW_INDEP_INFO from that struct...

Handle return codes from OV5640_Init, OC5640_create_sysfs_attributes,
initCamera, if those fail,

OV5640.c
@@ -113,7 +113,7 @@ static const struct attribute_group ov5640_groups = {
 };
 
 
-void OV5640_create_sysfs_attributes(struct device *dev)
+int OV5640_create_sysfs_attributes(struct device *dev)
 {
 	int ret;
 	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev->driver_data;
@@ -122,6 +122,7 @@ void OV5640_create_sysfs_attributes(struct device *dev)
 	ret = sysfs_create_group(&pdev->dev.kobj, &ov5640_groups);
 	if (ret)
 		pr_err("failed to add sys fs entry\n");
+	return ret;
 
 }
 
@@ -660,11 +661,12 @@ DWORD OV5640_FlipImage(PCAM_HW_INDEP_INFO pInfo, bool flip)
 	return dwErr;
 }
 
-static BOOL initCamera(PCAM_HW_INDEP_INFO pInfo, BOOL fullInit, CAM_NO camera)
+static int initCamera(struct device *dev, CAM_NO camera)
 {
+	PCAM_HW_INDEP_INFO pInfo = dev->driver_data;
 	BOOL ret = 0; //ERROR_SUCCESS
 	struct platform_device *pdev = pInfo->pLinuxDevice;
-	struct device *dev = &pdev->dev;
+
 
 	/* Read the OTP memory before the initial configuration. This
 	 * is the only time the otp memory is read. If read after the
@@ -737,17 +739,20 @@ BOOL OV5640_reinit(PCAM_HW_INDEP_INFO pInfo)
 	return TRUE;
 }
 
-BOOL OV5640_Init(PCAM_HW_INDEP_INFO pInfo)
+int OV5640_Init(struct device *dev)
 {
+	PCAM_HW_INDEP_INFO pInfo = dev->driver_data;
+
 	INIT_WORK(&pInfo->nightmode_work, nightmode_on_off_work);
+	int ret = 0;
 
 	if (pInfo->cameraI2CAddress[1] == 0)	//Only 1 active camera
 		g_camera = CAM_1;
 
-	initCamera(pInfo, TRUE, g_camera);
+	ret = initCamera(dev, g_camera);
 
 	init = 1;
-	return TRUE;
+	return ret;
 }
 
 DWORD OV5640_IOControl(PCAM_HW_INDEP_INFO pInfo,

vcam_eoco.c
@@ -106,15 +106,27 @@ DWORD EocoInitHW(struct device *dev)
 	}
 
 	EnablePower(pInfo, TRUE);
-	OV5640_Init(pInfo);
-	OV5640_create_sysfs_attributes(dev);
+	ret = OV5640_Init(dev);
+	if (ret) {
+		goto out_init;
+	}
+
+	ret = OV5640_create_sysfs_attributes(dev);
+	if (ret) {
+		goto out_sysfs;
+	}
+
+	return ret;
+out_sysfs:
+	EnablePower(pInfo, FALSE);
+out_init:
+
 	return ret;
 }
 
 DWORD EocoDeInitHW(struct device *dev)
 {
 	PCAM_HW_INDEP_INFO pInfo = dev->driver_data;
-	dev_err(dev, "%s", __func__);
 	OV5640_remove_sysfs_attributes(dev);
 	/* OV5640_DeInit(pInfo); */
 	EnablePower(pInfo, FALSE);

vcamd.c
@@ -109,9 +109,10 @@ static int vcam_probe(struct platform_device *pdev)
 			ret = 0;
 		}
 
-	if (ret)
+	if (ret) {
 		dev_err(dev, "failed to init hardware!\n");
-
+		misc_deregister(&vcam_miscdev);
+	}
 	return ret;
 }
 
@@ -349,7 +350,7 @@ static int VCAM_Open(struct inode *inode, struct file *filp)
 			/*      break; */
 
 		case OV5640:
-			OV5640_Init(gpDev);
+			OV5640_Init(dev);
 			break;
 
 		default:

OV5640.h
@@ -22,10 +22,6 @@
 
 typedef enum { CAM_1, CAM_2, CAM_ALL } CAM_NO;
 
-// Local definitions
-
-#define DEBUG_TMO           0
-
 // Local functions
 
 // Local variables
@@ -43,7 +39,7 @@ BOOL OV5640_DoI2CWrite(PCAM_HW_INDEP_INFO pInfo,
 		       struct reg_value *pMode, USHORT elements, CAM_NO camera);
 DWORD OV5640_FlipImage(PCAM_HW_INDEP_INFO pInfo, bool flip);
 void OV5640_enable_stream(PCAM_HW_INDEP_INFO pInfo, CAM_NO camera, bool enable);
-void OV5640_create_sysfs_attributes(struct device *dev);
+int OV5640_create_sysfs_attributes(struct device *dev);
 void OV5640_remove_sysfs_attributes(struct device *dev);
 
 static struct reg_value ov5640_setting_High_K[] = {

vcam_internal.h
@@ -77,7 +77,8 @@ BOOL OV7740_Init(PCAM_HW_INDEP_INFO pInfo);
 DWORD OV7740_IOControl(PCAM_HW_INDEP_INFO pInfo,
 		       DWORD Ioctl, PUCHAR pBuf, PUCHAR pUserBuf);
 
-BOOL OV5640_Init(PCAM_HW_INDEP_INFO pInfo);
+/* BOOL OV5640_Init(PCAM_HW_INDEP_INFO pInfo); */
+int OV5640_Init(struct device *dev);
 DWORD OV5640_IOControl(PCAM_HW_INDEP_INFO pInfo,
 		       DWORD Ioctl, PUCHAR pBuf, PUCHAR pUserBuf);
 void OV5640_MipiSuspend(PCAM_HW_INDEP_INFO pInfo, BOOL bSuspend);

commit cc18ae4f2ad3c923183c54b345aac018003b970e

EOWYN-2407: Fix compile errors on EC501 (driver_data)
This fixes an error introduced in commit 584c5adc516d... (EOWYN-2407
VCAM Configuration) where we access driver_data in  struct device *
directly. Which caused problems on EC501 platform.

Now, instead we access this structure using functions get/set-drvdata

OV5640.c
@@ -813,7 +813,7 @@ static ssize_t enable_stream_store(struct device *dev,
 			    const char *buf, size_t count)
 {
 	unsigned long val;
-	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev->driver_data;
+	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev_get_drvdata(dev);
 
 	if (kstrtoul(buf, 0, &val) < 0)
 		return -EINVAL;
@@ -827,7 +827,7 @@ static ssize_t flip_store(struct device *dev,
 			    const char *buf, size_t count)
 {
 	unsigned long val;
-	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev->driver_data;
+	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev_get_drvdata(dev);
 
 	if (kstrtoul(buf, 0, &val) < 0)
 		return -EINVAL;
@@ -855,7 +855,7 @@ static ssize_t mirror_enable_store(struct device *dev,
 			    const char *buf, size_t count)
 {
 	unsigned long val;
-	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev->driver_data;
+	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev_get_drvdata(dev);
 
 	if (kstrtoul(buf, 0, &val) < 0)
 		return -EINVAL;
@@ -867,7 +867,7 @@ static ssize_t mirror_enable_store(struct device *dev,
 static ssize_t autofocus_enable_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
 	unsigned long val;
-	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev->driver_data;
+	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev_get_drvdata(dev);
 
 	if (kstrtoul(buf, 0, &val) < 0)
 		return -EINVAL;
@@ -880,7 +880,7 @@ static ssize_t autofocus_enable_store(struct device *dev, struct device_attribut
 static ssize_t set_fov_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
 	unsigned long val;
-	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev->driver_data;
+	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev_get_drvdata(dev);
 
 	if (kstrtoul(buf, 0, &val) < 0)
 		return -EINVAL;
@@ -918,7 +918,7 @@ static const struct attribute_group ov5640_groups = {
 int OV5640_create_sysfs_attributes(struct device *dev)
 {
 	int ret;
-	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev->driver_data;
+	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev_get_drvdata(dev);
 	struct platform_device *pdev = pInfo->pLinuxDevice;
 
 	ret = sysfs_create_group(&pdev->dev.kobj, &ov5640_groups);
@@ -930,7 +930,7 @@ int OV5640_create_sysfs_attributes(struct device *dev)
 
 void OV5640_remove_sysfs_attributes(struct device *dev)
 {
-	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev->driver_data;
+  PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev_get_drvdata(dev);
 	struct platform_device *pdev = pInfo->pLinuxDevice;
 	sysfs_remove_group(&pdev->dev.kobj, &ov5640_groups);
 }
@@ -1504,7 +1504,7 @@ static int OV5640_set_fov(PCAM_HW_INDEP_INFO pInfo, CAM_NO camera, int fov)
  */
 static void OV5640_Testpattern_Enable(struct device *dev, bool on)
 {
-	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev->driver_data;
+  PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev_get_drvdata(dev);
 
 	if (on) {
 		dev_info(dev, "Enable testpattern\n");
@@ -1546,7 +1546,7 @@ DWORD OV5640_FlipImage(PCAM_HW_INDEP_INFO pInfo, bool flip)
  */
 static int initMIPICamera(struct device *dev, CAM_NO camera)
 {
-	PCAM_HW_INDEP_INFO pInfo = dev->driver_data;
+  PCAM_HW_INDEP_INFO pInfo = dev_get_drvdata(dev);
 	int ret = 0;
 
 	dev_info(dev, "mipi interface\n");
@@ -1597,7 +1597,7 @@ static int initMIPICamera(struct device *dev, CAM_NO camera)
  */
 static int initCSICamera(struct device *dev, CAM_NO camera)
 {
-	PCAM_HW_INDEP_INFO pInfo = dev->driver_data;
+	PCAM_HW_INDEP_INFO pInfo = dev_get_drvdata(dev);
 	int ret = 0;
 
 	dev_info(dev, "cam %lu, Parallell interface\n", camera);
@@ -1629,7 +1629,7 @@ static int initCSICamera(struct device *dev, CAM_NO camera)
  */
 static int initCamera(struct device *dev, CAM_NO camera)
 {
-	PCAM_HW_INDEP_INFO pInfo = dev->driver_data;
+	PCAM_HW_INDEP_INFO pInfo = dev_get_drvdata(dev);
 	int ret = 0;
 
 	/* Read the OTP memory before the initial configuration. This
@@ -1677,7 +1677,7 @@ int OV5640_reinit(PCAM_HW_INDEP_INFO pInfo)
  */
 int OV5640_Init(struct device *dev)
 {
-	PCAM_HW_INDEP_INFO pInfo = dev->driver_data;
+	PCAM_HW_INDEP_INFO pInfo = dev_get_drvdata(dev);
 	int ret = 0;
 
 	INIT_WORK(&pInfo->nightmode_work, nightmode_on_off_work);

vcam_eoco.c
@@ -51,7 +51,7 @@ DWORD EocoInitHW(struct device *dev)
 {
 	int ret = 0;
 
-	PCAM_HW_INDEP_INFO pInfo = dev->driver_data;
+	PCAM_HW_INDEP_INFO pInfo = dev_get_drvdata(dev);
 	struct platform_device *pdev = pInfo->pLinuxDevice;
 
 	pInfo->hI2C = i2c_get_adapter(0);
@@ -132,7 +132,7 @@ out_init:
 
 DWORD EocoDeInitHW(struct device *dev)
 {
-	PCAM_HW_INDEP_INFO pInfo = dev->driver_data;
+	PCAM_HW_INDEP_INFO pInfo = dev_get_drvdata(dev);
 	OV5640_remove_sysfs_attributes(dev);
 	/* OV5640_DeInit(pInfo); */
 	EnablePower(pInfo, FALSE);
@@ -251,20 +251,20 @@ DWORD SetTorchState(PCAM_HW_INDEP_INFO pInfo, VCAMIOCTLFLASH *pFlashData)
 
 static int eodp_vcam_enable_clk(struct device *dev, int on)
 {
-	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev->driver_data;
+  PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev_get_drvdata(dev);
 	return gpio_direction_output(pInfo->clk_en_gpio, on);
 }
 
 static int eodp_vcam_powerdown(struct device *dev, int on)
 {
-	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev->driver_data;
+  PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev_get_drvdata(dev);
 	return gpio_direction_output(pInfo->pwdn_gpio, on);
 }
 
 
 static int eodp_vcam_reset(struct device *dev, int on)
 {
-	PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev->driver_data;
+  PCAM_HW_INDEP_INFO pInfo = (PCAM_HW_INDEP_INFO)dev_get_drvdata(dev);
 	return gpio_direction_output(pInfo->reset_gpio, on);
 }
 

vcamd.c
@@ -69,7 +69,7 @@ static int vcam_probe(struct platform_device *pdev)
 		return -ENOMEM;
 	}
 
-	dev->driver_data=gpDev;
+	dev_set_drvdata(dev, gpDev);
 	platform_set_drvdata(pdev, gpDev);
 	gpDev->pLinuxDevice = pdev;
 